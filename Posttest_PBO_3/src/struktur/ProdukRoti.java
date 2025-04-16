252998ea6fee5839a92e9ac2170dac2e9fc1d6bc
RaflyOlanda <162123130+RaflyOlanda@users.noreply.github.com>
2025-04-16 16:53:37 +0800

    Add files via upload

---
+++ Posttest_PBO_3/src/struktur/ProdukRoti.java
@@ -0,0 +1,23 @@
+package struktur;
+
+public class ProdukRoti extends Produk {
+    private String rasa;
+
+    public ProdukRoti(String nama, double harga, int stok, Kategori kategori, String rasa) {
+        super(nama, harga, stok, kategori);
+        this.rasa = rasa;
+    }
+
+    public String getRasa() {
+        return rasa;
+    }
+
+    public void setRasa(String rasa) {
+        this.rasa = rasa;
+    }
+
+    @Override
+    public String getDetailTambahan() {
+        return " - Rasa: " + rasa;
+    }
+}
